7ff8be13f028ffd55105843a4b1736a24b46a2cb
mur4ik18 <o.o.kotov1999@gmail.com>
2023-05-19 20:22:13 +0200

    :rocket: added sway, nvim and waybar configs

---
+++ get.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+~/.config/arch-.files/
+# mouve sway config
+cp ~/.config/sway/* ~/.config/arch-.files/sway/
+
+# mouve waybar config
+cp ~/.config/waybar/* ~/.config/arch-.files/waybar/
+
+# mouve nvim config
+cp ~/.config/nvim/* ~/.config/arch-.files/nvim/
